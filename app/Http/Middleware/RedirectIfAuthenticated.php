17dcd436e08f2b0ca7fe88f8a5ac93242909acb4
Himanshu Batra <himanshu@batra.io>
2020-05-30 16:03:58 -0400

    WiP

---
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace WebApp\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use WebApp\App\Providers\RouteServiceProvider;
+
+class RedirectIfAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+
+        if (Auth::guard($guard)->check()) {
+            return redirect(RouteServiceProvider::HOME);
+        }
+
+        return $next($request);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Frontend
+
+Route::group(['namespace' => 'Pages\Controllers'], function () {
+    Route::get('/', 'PagesController@index')->name('home');
+});
+
+
+// Backend
+
+Route::group(['prefix' => 'user', 'namespace' => 'Dashboard\Controllers'], function () {
+    Route::get('/', 'UserController@index')->name('user.home');
+});
+
+
+Route::group(['prefix' => 'admin', 'Dashboard\Controllers'], function () {
+    Route::get('/', 'AdminController@index')->name('admin.home');
+});
+
+
+
+Route::group(['namespace' => 'Auth\Controllers'], function () {
+    Route::get('login', 'LoginController@showLoginForm')->name('login');
+    Route::post('login', 'LoginController@login');
+    Route::post('logout', 'LoginController@logout')->name('logout');
+    // Registration Routes...
+    Route::get('register', 'RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'RegisterController@register');
+});
+
+
+Route::get('/home', 'HomeController@index')->name('home');
